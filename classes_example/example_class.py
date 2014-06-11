7487e165cd44f518a3da28fe6ea489ea32bdb7f9
senthil rajasekharan <senthil.rajasekharan@dowjones.com>
2014-06-11 17:39:10 -0500

    ** bound method objects

--- classes_example/example_class.py
+++ classes_example/example_class.py
@@ -9,14 +9,22 @@ class ExampleClass:
             "db_user" : "reader",
             "db_password" : "reader"
             }
-    def example_function():
+
+    def __init__(self, db_host):
+        if db_host:
+            self.config_data['db_host'] = db_host
+
+    def example_function( self ):
         return "This is an example function."
 
 def main():
     print ExampleClass.config_data
-    print ExampleClass.example_function 
     print ExampleClass.__doc__
     print ExampleClass.config_data['db_host']
+    print ExampleClass('localdb2.abccorp.net').config_data['db_host']
+    print ExampleClass.config_data['db_host']
+    print ExampleClass.example_function 
+    print ExampleClass(None).example_function
 
 
 if __name__ == '__main__':
