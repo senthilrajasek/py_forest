63b25eb5851a02dad7f9970ca118f29fc6dfc700
senthil <senthilblogs@gmail.com>
2014-08-14 22:33:32 -0700

    ** flatten example

--- recursion_examples/flatten.py
+++ recursion_examples/flatten.py
@@ -1,6 +1,16 @@
 import sys
 
+def flatten( input_array ):
+	if type( input_array ) == int:
+		return [input_array]
+	else:
+		if len( input_array ) > 1:
+			return flatten( input_array[0] ) + flatten( input_array[1:] )
+		else:
+			return flatten( input_array[0] )
+
+
 def main():
-	print 'hello '
+	print flatten( [1, 3, [5, 7, 11], 13, 17, [19]] )
 if __name__ == '__main__':
 	sys.exit( main() )
